308cbf4521f95ea174493b4271220260f64bef40
kcneinstedt <kcneinstedt@mercercounty.org>
2024-02-09 15:28:53 -0500

    Changed field data type checking to occur once within the arcpy.ListFields() function rather than once per field per record in SearchCursor. Added a super class for checking fields and subclasses, one for each field in RCL.

--- objects.py
+++ objects.py
@@ -6,7 +6,7 @@ class Bool0:
         pass
 
     def __call__(self, *args, **kwargs):
-        print("1true")
+        print("0 true")
         return bool(*args)
 
 
@@ -14,6 +14,172 @@ class Bool1:
     def __init__(self):
         pass
 
-    def __call__(self, *args, **kwargs):
-        print("2true")
-        return bool(*args)
+    def __call__(self, value, **kwargs):
+        print("1 true")
+        self.right_type = int(value) == value
+
+
+def is_correct_type(value, desired_type: type) -> bool:
+    return type(value) == desired_type
+
+
+class FieldCheck:
+    def __init__(self):
+        self.desired_type = str
+        self.allow_null = False
+
+    def is_null(self, value, allow: bool) -> bool:
+        return type(value) == type(None)
+
+    def __call__(self, value):
+        print(is_correct_type(value, self.desired_type))
+        print(type(value))
+        print("checked type")
+        print(self.allow_null == self.is_null(value))
+        print("checked null")
+        return is_correct_type(value, self.desired_type)
+
+
+class ExternalStreetKey(FieldCheck):
+    def __init__(self):
+        super().__init__()
+
+
+class Shape(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = tuple
+
+
+class StreetName(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class LeftFromAddress(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = int
+
+
+class LeftToAddress(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = int
+
+
+class RightFromAddress(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = int
+
+
+class RightToAddress(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = int
+
+
+class LeftParity(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class RightParity(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class LeftCityCode(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class RightCityCode(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class LeftCountyCode(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class RightCountyCode(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class LeftState(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class RightState(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class FeatureTypeCode(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class SpeedLimit(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = int
+
+
+class LeftZipCode(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class RightZipCode(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class OneWayCode(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class ToElevation(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = int
+
+
+class FromElevation(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = int
+
+
+class LocationName(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
+class RoutingStreetExternalKey(FieldCheck):
+    def __init__(self):
+        super().__init__()
+        self.desired_type = str
+
+
