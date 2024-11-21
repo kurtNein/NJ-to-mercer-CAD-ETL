bfcfd70cbad40fd498d6ba2c4ca4888ce3b3f6dd
kcneinstedt <kcneinstedt@mercercounty.org>
2024-11-21 11:15:48 -0500

    removed CheckGeometry in transform method, added calcs for left/right zip codes in NJ_RCL

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from address_points import NJ_AP
+from road_centerlines import NJ_RCL
 import arcpy
 
 arcpy.env.workspace = r'C:\Users\kcneinstedt\Downloads\CAD Output\AddressPoints_CAD.gdb'
 
 nj_ap_layer = r'NJAddressPoints'
+nj_rcl_layer = r'NJ_RCL_NOT_NULL'
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    nj_ap = NJ_AP(arcpy.env.workspace, nj_ap_layer)
-    nj_ap.transform()
+    a = input(f"Transform address point layer {nj_ap_layer}? Y/N: ")
+    if a == "Y":
+        nj_ap = NJ_AP(arcpy.env.workspace, nj_ap_layer)
+        nj_ap.transform()
+
+    a = input(f"Transform road centerline layer {nj_rcl_layer}? Y/N: ")
+    if a == "Y":
+        nj_rcl = NJ_RCL(arcpy.env.workspace, nj_rcl_layer)
+        nj_rcl.transform()
